1f63cd687ac1c173ffd50b415d485fc03556ebd1
tomerbibi <66681821+tomerbibi@users.noreply.github.com>
2021-01-12 16:08:06 +0200

    Add files via upload

---
+++ Test_Part_3/Categories.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Part_3
+{
+    class Categories
+    {
+        public Categories()
+        {
+        }
+
+        public Categories(long iD, string name)
+        {
+            ID = iD;
+            Name = name;
+        }
+
+        public long ID { get; set; }
+        public string Name { get; set; }
+    }
+}
